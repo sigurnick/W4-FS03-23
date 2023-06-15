c9f6c4721fd0853d50819e1133d89e063943ea85
sigurnick <nicolas94.m@gmail.com>
2023-06-15 08:11:04 +0200

    1.0.1

---
+++ question-script-2.js
@@ -0,0 +1,165 @@
+const questions = [
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "What does CPU stand for?",
+      correct_answer: "Central Processing Unit",
+      incorrect_answers: [
+        "Central Process Unit",
+        "Computer Personal Unit",
+        "Central Processor Unit",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+      correct_answer: "Final",
+      incorrect_answers: ["Static", "Private", "Public"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "The logo for Snapchat is a Bell.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question:
+        "Pointers were not used in the original C programming language; they were added later on in C++.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the most preferred image format used for logos in the Wikimedia database?",
+      correct_answer: ".svg",
+      incorrect_answers: [".png", ".jpeg", ".gif"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "In web design, what does CSS stand for?",
+      correct_answer: "Cascading Style Sheet",
+      incorrect_answers: [
+        "Counter Strike: Source",
+        "Corrective Style Sheet",
+        "Computer Style Sheet",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the code name for the mobile operating system Android 7.0?",
+      correct_answer: "Nougat",
+      incorrect_answers: [
+        "Ice Cream Sandwich",
+        "Jelly Bean",
+        "Marshmallow",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "On Twitter, what is the character limit for a Tweet?",
+      correct_answer: "140",
+      incorrect_answers: ["120", "160", "100"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "Linux was first created as an alternative to Windows XP.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "Which programming language shares its name with an island in Indonesia?",
+      correct_answer: "Java",
+      incorrect_answers: ["Python", "C", "Jakarta"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "hard",
+        question:
+          "Prova domanda difficile?",
+        correct_answer: "Java",
+        incorrect_answers: ["Python", "C", "Jakarta"],
+      },
+  ];
+
+
+
+//global variables
+let score = 0
+let difficult
+questionContainer = document.getElementById("question")
+answersContainer = document.getElementById("answers")
+
+const choseDfficult = (e) => {
+if(e.target.innerText === "Easy"){
+    difficult = "Easy"
+}
+else{
+    difficult = "Hard"
+}
+console.log(difficult);
+return difficult
+}
+
+//function request difficult and number questions to user
+const userInput = () => {
+    let question = document.createElement('h1')
+    question.innerText = "Chose the quiz difficult"
+    questionContainer.appendChild(question)
+
+    //ask difficult
+    let answer1Container = document.createElement('div')
+    let answer2Container = document.createElement('div')
+    let answer1 = document.createElement('h3')
+    let answer2 = document.createElement('h3')
+    answer1.innerText = "Easy"
+    answer2.innerText = "Hard"
+
+    answersContainer.appendChild(answer1Container).appendChild(answer1)
+    answersContainer.appendChild(answer2Container).appendChild(answer2)
+
+    difficult = answer1.addEventListener("click", choseDfficult)
+    difficult = answer2.addEventListener("click", choseDfficult)
+    
+
+    
+    
+
+
+//     let answer = document.createElement('form')
+//     answer.innerHTML = `
+//     <form>
+//   <label for="quantity">Quantity (between 2 and ${}):</label>
+//   <input type="number" id="quantity" name="quantity" min="1" max="5">
+//   <input type="submit" value="Submit">
+// </form>`
+
+}
+
+userInput()
+
